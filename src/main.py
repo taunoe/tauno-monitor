faa760fc48cc655e41d50a77985e8b0d6f7b6ac0
Tauno Erik <sedumacre@gmail.com>
2025-06-25 21:47:33 +0300

    test

--- src/main.py
+++ src/main.py
@@ -29,7 +29,7 @@ from .preferences import TaunoPreferencesWindow
 import os
 import gettext, locale
 
-APP_VERSION = '0.2.3'
+APP_VERSION = '0.2.4'
 
 class TaunoMonitorApplication(Adw.Application):
     """The main application singleton class."""
