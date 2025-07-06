d7dfd6570bf23a0612d72c7c84ebf5d94e896370
Tauno Erik <sedumacre@gmail.com>
2025-07-06 18:34:46 +0300

    ger translation

--- src/main.py
+++ src/main.py
@@ -29,7 +29,7 @@ from .preferences import TaunoPreferencesWindow
 import os
 import gettext, locale
 
-APP_VERSION = '0.2.6'
+APP_VERSION = '0.2.7'
 
 class TaunoMonitorApplication(Adw.Application):
     """The main application singleton class."""
